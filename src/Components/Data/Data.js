9af47ee279fa71e032d3d3922d8d0b16f7cd81a7
Ulil Albab <ulilalbabcmpk@gmail.com>
2025-04-15 16:43:50 +0700

    add data and sidebar

---
+++ src/Components/Data/Data.js
@@ -0,0 +1,94 @@
+const Data = [
+    {
+        id: 1,
+        image: "../assets/1.png",
+        title: "Big 4 Auditor Financial Analyst",
+        description: "Mulai transformasi dengan instruktur profesional, harga yang terjangkau, dan...",
+        name: "Kaluna",
+        position: "Senior Accountant di Gojek",
+        rating: "3.5(86)",
+        price: "Rp 300K"
+    },
+    {
+        id: 2,
+        image: "../assets/2.png",
+        title: "Big 4 Auditor Financial Analyst",
+        description: "Mulai transformasi dengan instruktur profesional, harga yang terjangkau, dan...",
+        name: "Ulil",
+        position: "Senior Accountant di Gojek",
+        rating: "3.5(86)",
+        price: "Rp 650K"
+    },
+    {
+        id: 3,
+        image: "../assets/3.png",
+        title: "Big 4 Auditor Financial Analyst",
+        description: "Mulai transformasi dengan instruktur profesional, harga yang terjangkau, dan...",
+        name: "Prima",
+        position: "Senior Accountant di Gojek",
+        rating: "3.5 (86)",
+        price: "Rp 499K"
+    },
+    {
+        id: 4,
+        image: "../assets/4.png",
+        title: "Big 4 Auditor Financial Analyst",
+        description: "Mulai transformasi dengan instruktur profesional, harga yang terjangkau, dan...",
+        name: "Shaka",
+        position: "Senior Accountant di Gojek",
+        rating: "3.5 (86)",
+        price: "Rp 899K"
+    },
+    {
+        id: 5,
+        image: "../assets/5.png",
+        title: "Big 4 Auditor Financial Analyst",
+        description: "Mulai transformasi dengan instruktur profesional, harga yang terjangkau, dan...",
+        name: "Jason",
+        position: "Senior Accountant di Gojek",
+        rating: "3.5 (86)",
+        price: "Rp 139K"
+    },
+    {
+        id: 6,
+        image: "../assets/6.png",
+        title: "Big 4 Auditor Financial Analyst",
+        description: "Mulai transformasi dengan instruktur profesional, harga yang terjangkau, dan...",
+        name: "Nanda",
+        position: "Senior Accountant di Gojek",
+        rating: "3.5 (86)",
+        price: "Rp 189K"
+    },
+    {
+        id: 7,
+        image: "../assets/7.png",
+        title: "Big 4 Auditor Financial Analyst",
+        description: "Mulai transformasi dengan instruktur profesional, harga yang terjangkau, dan...",
+        name: "Nisa",
+        position: "Senior Accountant di Gojek",
+        rating: "3.5 (86)",
+        price: "Rp 359K"
+    },
+    {
+        id: 8,
+        image: "../assets/8.png",
+        title: "Big 4 Auditor Financial Analyst",
+        description: "Mulai transformasi dengan instruktur profesional, harga yang terjangkau, dan...",
+        name: "Michael",
+        position: "Senior Accountant di Gojek",
+        rating: "3.5 (86)",
+        price: "Rp 100K"
+    },
+    {
+        id: 9,
+        image: "../assets/9.png",
+        title: "Big 4 Auditor Financial Analyst",
+        description: "Mulai transformasi dengan instruktur profesional, harga yang terjangkau, dan...",
+        name: "Jacob",
+        position: "Senior Accountant di Gojek",
+        rating: "3.5 (86)",
+        price: "Rp 250K"
+    }
+]
+
+export default Data
--- src/Components/Fragments/Category.jsx
+++ src/Components/Fragments/Category.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
+import dataset from '../Data/Data';
+import { data } from 'react-router';
 import CardLayouts from '../Layouts/CardLayouts';
 
 const Dropdown = ({ title, options }) => {
@@ -16,18 +18,18 @@ const Dropdown = ({ title, options }) => {
   };
 
   return (
-    <div className="relative inline-block text-left">
-      <div className='w-full'>
+    <div className=" text-left">
+      <div className=''>
         <button
           onClick={toggleDropdown}
-          className="inline-flex justify-between w-full text-green-500 cursor-pointer` rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          className="inline-flex justify-between w-full cursor-pointer` rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         >
           {selectedOption || title}
           {
             isOpen ? (
-              <FaChevronDown className="ml-2 -mr-1 h-5 w-5 text-green-500" aria-hidden="true" />
-            ) : (
               <FaChevronUp className="ml-2 -mr-1 h-5 w-5 text-green-500" aria-hidden="true" />
+            ) : (
+              <FaChevronDown className="ml-2 -mr-1 h-5 w-5 text-green-500" aria-hidden="true" />
             )
           }
         </button>
@@ -56,17 +58,21 @@ const Category = () => {
   const options1 = ['Pemasaran', 'Digital & Teknologi', 'Pengembangan Diri', 'Bisnis Manajemen'];
   const options2 = ['Pemasaran', 'Digital & Teknologi', 'Pengembangan Diri', 'Bisnis Manajemen'];
   const options3 = ['Kurang dari 4 Jam', '4 - 8 Jam', 'Lebih dari 8 Jam'];
+  const options4 = ['Harga Rendah, Harga tinggi, A to X, Z to A, Rating Tertinggi, Rating Terendah'];
 
   return (
-    <div className='bg-primary'>
+    <div className='bg-primary lg:px-5'>
         <h1 className='font-bold text-2xl pl-5 pt-5'>
             Koleksi Video Pembelajaran Unggulan
         </h1>
         <p className='text-gray-600 pl-5'>
             Jelajahi Dunia Pengetahuan Melalui Pilihan Kami!
         </p>
-        <div className='grid grid-cols-1 lg:grid-cols-2 p-5'>
-            <div className="flex flex-col bg-white space-y-4 border border-gray-200 p-5">
+        <div className='w-full p-5'>
+          <div className='lg:flex lg:justify-between lg:h-85'>
+
+            {/* sidebar */}
+            <div className="lg:w-1/4 flex flex-col bg-white space-y-4 border border-gray-200 p-5">
               <div className='flex justify-between'>
                 <p className='font-bold'>
                   Filter
@@ -79,9 +85,21 @@ const Category = () => {
                 <Dropdown title="Harga" options={options2} />
                 <Dropdown title="Durasi" options={options3} />
             </div>
-            <div>
-                {/* <CardLayouts></CardLayouts> */}
+
+            {/* content */}
+            <div className='flex justify-between mt-5 lg:mt-0 lg:gap-10'>
+                <Dropdown title="Urutkan" options={options3} />
+                <search>
+                  <form>
+                    <input name="search" id="search" placeholder="Cari Kelas" className='border border-gray-300 rounded-md px-3 py-2 w-40 font-bold'/>
+                  </form>
+                </search>
+                {/* <div>
+                  ewgew
+                </div> */}
+              {/* <CardLayouts></CardLayouts> */}
             </div>
+          </div>
         </div>
     </div>
   );
